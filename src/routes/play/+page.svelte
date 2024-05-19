f9090a71ceec02667ff4f39cf264508cc3270d82
slacktracer <slacktracer@gmail.com>
2024-05-19 15:39:45 -0300

    Requesting fullscreen.

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,12 @@
 <script>
+  import { browser } from "$app/environment";
+
   import OPortal from "../../lib/runes/components/OPortal/OPortal.svelte";
   import Portals from "../../lib/runes/components/Portals/Portals.svelte";
+
+  if (browser) {
+    document.body.requestFullscreen();
+  }
 </script>
 
 <Portals>
