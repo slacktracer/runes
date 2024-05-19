3993f6c8d2033fc74d79819369b15c8396a5fb13
slacktracer <slacktracer@gmail.com>
2024-05-18 23:47:21 -0300

    Vite config code is essential for local websocket developement.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { PluginOption } from "vite";
+import { defineConfig } from "vitest/config";
+
+import { webSocketServerVitePlugin } from "./src/lib/runes/server/web-socket-server/web-socket-server-vite-plugin.js";
+
+const reloadEverything: PluginOption = {
+  handleHotUpdate: ({ server }) => {
+    server.restart().then(() => server.hot.send({ type: "full-reload" }));
+
+    return [];
+  },
+
+  name: "reload-everything",
+};
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit(), webSocketServerVitePlugin, reloadEverything],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
