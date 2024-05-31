133d72f87548674b6d56e467e6d188e37932366b
slacktracer <slacktracer@gmail.com>
2024-05-31 20:30:12 -0300

    No need for fulscreen right now...

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { browser } from "$app/environment";
+  // import { browser } from "$app/environment";
 
   import Play from "../../lib/runes/components/Play/Play.svelte";
 
-  if (browser) {
-    document.body.requestFullscreen().catch(console.warn);
-  }
+  // if (browser) {
+  //   document.body.requestFullscreen().catch(console.warn);
+  // }
 </script>
 
 <Play></Play>
