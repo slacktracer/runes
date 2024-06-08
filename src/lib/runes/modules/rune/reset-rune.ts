1d5a74a3e5f3f711a80d8ee16655c1598fc61992
slacktracer <slacktracer@gmail.com>
2024-06-08 19:54:23 -0300

    Much more fine-grained rune resetting.

--- src/lib/runes/modules/rune/reset-rune.ts
+++ src/lib/runes/modules/rune/reset-rune.ts
@@ -1,5 +1,14 @@
 import type { Rune } from "../../types/Rune.js";
-import { makeRune } from "./make-rune/make-rune.js";
+import { makeAnimations } from "./make-rune/make-animations";
+import { makeOutOfBounds } from "./make-rune/make-out-of-bounds";
+import { makeRendering } from "./make-rune/make-rendering";
 
-export const resetRune = ({ rune }: { rune: Rune }) =>
-  Object.assign(rune, makeRune({ dimensions: rune.dimensions }));
+export const resetRune = ({ rune }: { rune: Rune }) => {
+  rune.vertices.length = 0;
+
+  return Object.assign(rune, {
+    animations: makeAnimations(),
+    outOfBounds: makeOutOfBounds(),
+    rendering: makeRendering(),
+  });
+};
