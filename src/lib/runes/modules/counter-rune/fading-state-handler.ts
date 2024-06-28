f9a0c88cfe2975b551ea8b6c198f7f7a7de38dd3
slacktracer <slacktracer@gmail.com>
2024-06-28 19:43:51 -0300

    Allow turning in the fading state of counter rune.

--- src/lib/runes/modules/counter-rune/fading-state-handler.ts
+++ src/lib/runes/modules/counter-rune/fading-state-handler.ts
@@ -1,13 +1,21 @@
+import { mainEventBus } from "../../main-event-bus";
 import type { CounterRune } from "../../types/CounterRune";
 import { resetCounterRune } from "./reset-counter-rune";
 
 export const fadingStateHandler = ({
   counterRune,
-  timestamp,
 }: {
   counterRune: CounterRune;
   timestamp: number;
 }) => {
+  if (counterRune.input.touchEnd) {
+    if (mainEventBus) {
+      mainEventBus.emit("turn");
+
+      return;
+    }
+  }
+
   if (counterRune.rendering.thickness === 1) {
     counterRune.state.send({ type: "end" });
 
